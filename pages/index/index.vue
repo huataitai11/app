c80bad2b7be015c5b9c9e2d076ec4f093d254e3a
huataitai <2211373155@qq.com>
2022-11-02 01:12:27 +0800

    完成基础布局

---
+++ pages/index/index.vue
@@ -0,0 +1,30 @@
+<template>
+	<view class="index">
+		<view class="filter-active-color">
+			文字
+		</view>
+		<view class="iconfont icon-xiaoxi">
+			1111
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
